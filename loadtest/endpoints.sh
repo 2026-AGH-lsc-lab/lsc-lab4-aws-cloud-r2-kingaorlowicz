98e3e14c87be08a7eeb22c31252c56c7a0a37daa
kingaorlowicz <kinga.orlowicz@gmail.com>
2026-03-29 12:02:20 +0000

    first commit

--- loadtest/endpoints.sh
+++ loadtest/endpoints.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Endpoint URLs for load testing
 # Fill in after deploying with the URLs printed by each deploy script
-export LAMBDA_ZIP_URL=""        # e.g. https://<id>.lambda-url.us-east-1.on.aws
-export LAMBDA_CONTAINER_URL=""  # e.g. https://<id>.lambda-url.us-east-1.on.aws
-export FARGATE_URL=""           # e.g. http://<alb-dns>.us-east-1.elb.amazonaws.com
-export EC2_URL=""               # e.g. http://<public-ip>:8080
+export LAMBDA_ZIP_URL="https://nj4d753a2uovvkmlqxpduz6qp40iucaw.lambda-url.us-east-1.on.aws"        # e.g. https://<id>.lambda-url.us-east-1.on.aws
+export LAMBDA_CONTAINER_URL="https://ifzsquvv6sjnifhmuyj2hh3jam0mcjrt.lambda-url.us-east-1.on.aws"  # e.g. https://<id>.lambda-url.us-east-1.on.aws
+export FARGATE_URL="http://lsc-knn-alb-490713158.us-east-1.elb.amazonaws.com"           # e.g. http://<alb-dns>.us-east-1.elb.amazonaws.com
+export EC2_URL="http://32.195.204.47:8080"               # e.g. http://<public-ip>:8080
